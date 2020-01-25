5dc0d2de4fcf3bb71d2698737c67164709977255
ye1719365002 <1719365002@qq.com>
2020-01-25 14:35:20 +0800

    标题改变

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -88,7 +88,7 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App  this is my feature'
+      msg: 'Welcome to Your Vue.js App  this is my feature bbhhhhhfffggy'
     }
   }
 }
